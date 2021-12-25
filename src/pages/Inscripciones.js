8fb3f5da9f6e3ce2a98f9b13c7ca5a1756ef3f1b
Ignacio <nachonar2020@gmail.com>
2021-12-25 19:44:17 -0300

    commit

--- src/pages/Inscripciones.js
+++ src/pages/Inscripciones.js
@@ -13,12 +13,12 @@ const Inscripciones = () => {
         <StyledTitle>INSCRIPCIONES</StyledTitle>
         <StyledList>
           <StyledSubTitle>ACTUALES ESTUDIANTES DE LA INSTITUCIÓN</StyledSubTitle>
-          <StyledListItem><StyledLink href="https://deioweb.com.ar/SAE/">Contrato de Servicio Educativo 2022</StyledLink></StyledListItem>
+          <StyledListItem><StyledLink href="https://escuelacristoredentor.edu.ar/form2022/registro.html">Contrato de Servicio Educativo 2022</StyledLink></StyledListItem>
           <StyledRow/>
           <StyledSubTitle>INTERESADOS NUEVOS 2022</StyledSubTitle>
-          <StyledListItem><StyledLink href="http://credentor.dyndns.tv:8081/Academico/Login.aspx">Formulario Interesados NUEVOS INGRESOS NIVEL INICIAL 2022</StyledLink></StyledListItem>
-          <StyledListItem><StyledLink href="http://credentor.dyndns.tv:8081/Academico/Login.aspx">Formulario Interesados NUEVOS INGRESOS NIVEL PRIMARIO 2022</StyledLink></StyledListItem>
-          <StyledListItem><StyledLink href="http://credentor.dyndns.tv:8081/Academico/Login.aspx">Formulario Interesados NUEVOS INGRESOS NIVEL SECUNDARIO 2022</StyledLink></StyledListItem>
+          <StyledListItem><StyledLink href="https://docs.google.com/forms/d/e/1FAIpQLSc3ENvHlAqVPCvlkseGxUbAjpjnDcQpzQtGM9mEQI7SA8o4zg/viewform">Formulario Interesados NUEVOS INGRESOS NIVEL INICIAL 2022</StyledLink></StyledListItem>
+          <StyledListItem><StyledLink href="https://docs.google.com/forms/d/e/1FAIpQLSdGV3ZtZ6SLfMGZRzyHcnV_nEbc9SAY9oolNccsBMFTP83M2A/closedform">Formulario Interesados NUEVOS INGRESOS NIVEL PRIMARIO 2022</StyledLink></StyledListItem>
+          <StyledListItem><StyledLink href="https://docs.google.com/forms/d/e/1FAIpQLSdY4PPlF_hOHli23SeYSjn7DYN88tlRhcx5PFkkZaR_dMQXeQ/closedform">Formulario Interesados NUEVOS INGRESOS NIVEL SECUNDARIO 2022</StyledLink></StyledListItem>
         </StyledList>
       </StyledListWrapper>
       <Footer/>
